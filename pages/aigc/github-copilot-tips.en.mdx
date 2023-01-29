9fcdf8e52a50e01cc7f2a801b0f75a85dc7332b2
Hang <huhuhang@users.noreply.github.com>
2023-01-29 16:24:24 +0800

    init

---
+++ pages/aigc/github-copilot-tips.en.mdx
@@ -0,0 +1,23 @@
+# GitHub Copilot Tips
+
+You can use some tips to get the most out of GitHub Copilot.
+
+## Writing Comments First
+
+Writing comments in your code can help Copilot understand what you're trying to do. For example, if you're trying to create a function that adds two numbers, you can write a comment like this:
+
+```js
+// Add two numbers (GitHub Copilot adds the following)
+function add(a, b) {
+  return a + b;
+}
+```
+
+Copilot also works well when you write step verification scripts.
+
+```bash
+# check if the file exists (GitHub Copilot adds the following)
+test -f ~/hello.py
+# check if the file contains the string "Hello World"
+grep "Hello World" ~/hello.py
+```
---
+++ theme.config.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  useNextSeoProps() {
+    return {
+      titleTemplate: "%s – LabEx",
+    };
+  },
+  logo: (
+    <>
+      <svg width="15px" height="24px" viewBox="0 0 24 24">
+        <path d="M12.750877,6.48865231 C12.750877,6.48865231 7.3507833,13.4935398 5.62331208,16.7065086 C3.89584086,19.9200581 7.53412341,24 7.53412341,24 C7.53412341,24 13.2729597,15.2594241 13.9818748,14.247278 C15.9974519,11.3020082 12.750877,6.48807162 12.750877,6.48807162 M7.59290865,0 C7.59290865,0 2.19281499,7.00488749 0.465343772,10.218437 C-1.26212745,13.4325671 2.3761551,17.5119284 2.3761551,17.5119284 C2.3761551,17.5119284 8.11499143,8.77193322 8.8239065,7.75920639 C10.8394836,4.81393661 7.59290865,0 7.59290865,0"></path>
+      </svg>
+      <span style={{ marginLeft: ".2em", fontWeight: 600 }}>
+        LabEx Guidelines
+      </span>
+    </>
+  ),
+  faviconGlyph: "✍️",
+  footer: {
+    component: false,
+  },
+  editLink: {
+    component: null,
+  },
+  feedback: {
+    content: null,
+  },
+  i18n: [
+    { locale: "en", text: "English" },
+    { locale: "zh", text: "中文" },
+  ],
+};
+
+export default config;
